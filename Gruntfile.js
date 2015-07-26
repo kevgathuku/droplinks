e4e2be4453ee0f137febf515f79749abad6e3779
Kevin Ndung'u <kevgathuku@gmail.com>
2015-07-26 16:10:37 +0300

    Add grunt jshint task and lint relevant files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,15 @@ module.exports = function(grunt) {
       // build bundle.js from main.js
       'static/js/bundle.js': ['static/js/main.js']
     },
+    jshint: {
+      all: ['Gruntfile.js', 'static/js/main.js']
+    },
     watch: {
       files: [ 'static/js/main.js' ],
       tasks: [ 'browserify' ]
     }
-  })
-  grunt.loadNpmTasks('grunt-browserify')
-  grunt.loadNpmTasks('grunt-contrib-watch')
-}
+  });
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+};
