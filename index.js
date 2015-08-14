9448e93aa7d85484dd6fc9697f2b356f562681a2
Kevin Ndung'u <kevgathuku@gmail.com>
2015-08-14 16:26:42 +0300

    Specify process.env.PORT to run on Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Path = require('path');
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: process.env.PORT || 3000 });
 
 server.views({
   engines: {
